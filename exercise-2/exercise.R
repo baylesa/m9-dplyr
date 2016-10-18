6a66dd8dc748443613f101b26c342969df705b22
baylesa <andrewbayles1@gmail.com>
2016-10-18 15:19:26 -0700

    participation

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,32 +3,36 @@
 
 # Install devtools package: allows installations from GitHub
 install.packages('devtools')
-
+install.packages('dplyr')
 # Install "fueleconomy" package from GitHub
 devtools::install_github("hadley/fueleconomy")
 
 # Require/library the fueleconomy package
-
-
+library(dplyr)
+require(fueleconomy)
 # You should have have access to the `vehicles` data.frame
 
 
-# Create a data.frame of vehicles from 1997
-
+vehicles1997 <- filter(vehicles, year == 1997)
 
 # Use the `unique` function to verify that there is only 1 value in the `year` column of your new data.frame
-
+unique(vehicles1997)
 
 # Create a data.frame of 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
-
+two.wheel <- filter(vehicles1997, drive == "Front-Wheel Drive" & hwy > 20)
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
-
+worst.hwy <- filter(two.wheel, hwy == min(hwy))
 
 # Write a function that takes a `year` and a `make` as parameters, and returns 
 # The vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
+find.best <- function(make1, year1){
+  set1 <- filter(vehicles, year == year1 & make == make1)
+  best <- filter(set1, hwy == max(hwy))
+  return(best)
+}
 
-
+honda <- find.best("Honda", 1995)
 # What was the most efficient honda model of 1995?
 
 
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -4,18 +4,20 @@
 
 # Install devtools package: allows installations from GitHub
 install.packages('devtools')
-
+install.packages('dplyr')
+library('dplyr')
 # Install "fueleconomy" package from GitHub
 devtools::install_github("hadley/fueleconomy")
-
 # Require/library the fueleconomy package
-
+require(fueleconomy)
 
 # Which Accura model has the best hwy MPG in 2015? (without method chaining)
+set1 <- filter(vehicles, year == 2015 & make == "Acura")
+best.acura.noChain <- filter(set1, hwy == max(hwy))
 
 
 # Which Accura model has the best hwy MPG in 2015? (nesting functions)
-
+best.acura.nest <- select(filter())
 
 # Which Accura model has the best hwy MPG in 2015? (pipe operator)
 
