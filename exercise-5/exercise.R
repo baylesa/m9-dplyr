abf659748a6615574dddb9673f9f15605ec32098
baylesa <andrewbayles1@gmail.com>
2016-10-20 15:08:40 -0700

    check2

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -9,15 +9,21 @@ library(dplyr)
 require(nycflights13)
 # In which month was the average departure delay the greatest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
-nycflights13 <- read.csv(nycflights13)
+average.depart <- group_by(flights, month) %>% 
+                  summarise(mean = mean(dep_delay, na.rm = TRUE))%>%
+                  arrange()
+                  
+plot(average.depart)
+worst.month <- filter(max(average.depart))
 # If you create a data.frame with the columns "month", and "delay" above, you should be able to create 
 # a scatterplot by passing it to the 'plot' function
-View(nycflights13)
+View(flights)
 
 # In which airport were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
+average.delays <- group_by(flights, dest) %>% 
+                  summarise(mean = mean(arr_delay, na.rm = TRUE))
 
 ### Bonus ###
 # Which city was flown to with the highest average speed?
+highest
--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -2,13 +2,16 @@
 
 # Install the nycflights13 package and read it in. Require the dplyr package.
 # install.packages("nycflights13")
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
-
-
+View(flights)
+View(airports)
 # Create a dataframe of the average arrival delay for each destination, then use left_join
 # to join on the "airports" dataframe, which has the airport info
-
+arr.delay.avg <- group_by(flights, dest) %>%
+                 summarise(average.arr = mean(arr_delay, na.rm = TRUE))
+new.data <- left_join(arr.delay.avg, airports)
 
 # Create a dataframe of the average arrival delay for each airline, then use left_join
 # to join on the "airlines" dataframe, which has the airline info
